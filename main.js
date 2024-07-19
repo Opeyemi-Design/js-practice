a87bc8fe4cf2e471221d38833d34e9ce4ba61f93
Opeyemi Adetifa <opeyemiadetifa17@gmail.com>
2024-07-19 11:43:08 -0300

    add con m p

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ document.addEventListener('headerLoaded', () => {
       body.classList.toggle('nav-open');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const copyButton = document.getElementById('copy-email-address');
+    
+    copyButton.addEventListener('click', function() {
+      const email = this.getAttribute('data-email');
+      
+      // Copy email to clipboard
+      navigator.clipboard.writeText(email).then(function() {
+        // Change button text and style
+        copyButton.textContent = 'Email Address Copied';
+        copyButton.style.backgroundColor = 'var(--clr-neutral-500)';
+        copyButton.style.color = 'var(--clr-neutral-600)';
+        
+        // Optionally, revert the button after a few seconds
+        setTimeout(function() {
+          copyButton.textContent = 'Copy Email Address';
+          copyButton.style.backgroundColor = '';
+          copyButton.style.color = '';
+        }, 4000); // Revert after 3 seconds
+      }).catch(function(err) {
+        console.error('Failed to copy text: ', err);
+      });
+    });
+});
